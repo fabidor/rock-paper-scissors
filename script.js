59da51a5f5b16be789392bb9b0cfffee729e816a
fabidor <felixabidor@gmail.com>
2022-05-21 11:26:30 -0500

    Transition game to first to 3 Additionally, change switch statements into if-else statements for cleaner code, and add return values.

--- script.js
+++ script.js
@@ -2,51 +2,42 @@ let arr = ["rock", "paper", "scissors"]
 function computerChoose(){
     return arr[Math.floor(Math.random()*arr.length)];
 }
-let compChoice = computerChoose();
-console.log(compChoice);
-let personChoice = prompt("Choose Rock, Paper, or Scissors", "").toLowerCase();
 
-function RPS(compChoice, personChoice){
+function RPS(){
+    let compChoice = computerChoose();
+    console.log(compChoice);
+    let personChoice = prompt("Choose Rock, Paper, or Scissors", "").toLowerCase();
     console.log(personChoice);
     if (personChoice != "rock" && personChoice != "paper" && personChoice != "scissors"){
         console.log("You didn't put in one of the choices, numbnuts.");
-    } else if (personChoice == "rock"){
-        switch(compChoice){
-            case "rock": 
-                console.log("We are Evenly Matched I see");
-                break;
-            case "paper":
-                console.log("You Lose! Paper beats Rock");
-                break;
-            case "scissors":
-                console.log("You Win! Rock Beats Scissors");
-                break;
-        }
-    } else if(personChoice == "paper"){
-        switch(compChoice){
-            case "rock": 
-                console.log("You Win! Paper Beats Rock");
-                break;
-            case "paper":
-                console.log("We are Evenly Matched I see");
-                break;
-            case "scissors":
-                console.log("You Lose! Scissors beats Paper");
-                break;
-        }
-    } else if(personChoice == "scissors"){
-        switch(compChoice){
-            case "rock": 
-                console.log("You Lose! Rock Beats Scissors");
-                break;
-            case "paper":
-                console.log("You Win! Scissors Beats Paper");
-                break;
-            case "scissors":
-                console.log("We are Evenly Matched I see");
-                break;
-        }
+        return;
+    } else if (personChoice == compChoice){
+        console.log("Tie Game!");
+        return;
+    } else if(personChoice == "rock" && compChoice == "paper" || personChoice == "paper" && compChoice == "scissors" || personChoice == "scissors" && compChoice == "rock"){
+        console.log(`You Lose! ${compChoice} beats ${personChoice}.`);
+        return "CompWin";
+    } else{
+        console.log(`You Win! ${personChoice} beats ${compChoice}.`);
+        return "PersonWin";
     }
 }
 
-RPS(compChoice, personChoice);
+function fullGame(){
+    let compScore = 0;
+    let humScore = 0;
+    while(compScore < 3 && humScore < 3){
+        let result = RPS();
+        if (result == "CompWin"){
+            compScore ++;
+        } else if (result == "PersonWin"){
+            humScore ++;
+        }
+    }
+    if (compScore>humScore){
+        console.log("Computer Wins!")
+    } else{
+        console.log("You Win!")
+    }
+}
+fullGame();
